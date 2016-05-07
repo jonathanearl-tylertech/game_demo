5787d1d59b8344c2a5fa1b657479fed37d86924f
Jonathan Earl <metablue@uw.edu>
2016-05-07 13:24:54 -0700

    Starting_files

---
+++ Assets/Scripts/Hero_Interaction.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hero_Interaction : MonoBehaviour {
+
+    public float max_speed = 2f;
+    public float air_speed = 0.1f;
+    private Rigidbody2D rigid_body;
+
+    bool grounded = false;
+    public Transform ground_check;
+    float ground_radius = 0.5f;
+    public LayerMask what_is_ground;
+
+	// Use this for initialization
+	void Start () {
+        this.rigid_body = this.GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate () {
+        this.grounded = Physics2D.OverlapCircle(this.ground_check.position, this.ground_radius, this.what_is_ground);
+        float move = Input.GetAxis("Horizontal");
+        this.rigid_body.velocity = new Vector2(move * max_speed, this.rigid_body.velocity.y);
+
+        //if (this.rigid_body.velocity.x > max_speed && move < 0f)
+        //{
+        //    this.rigid_body.velocity = new Vector2(-this.rigid_body.velocity.x + move * air_speed, this.rigid_body.velocity.y);
+        //}
+        //else if (this.rigid_body.velocity.x < -max_speed && move > 0f)
+        //{
+        //    this.rigid_body.velocity = new Vector2(this.rigid_body.velocity.x + move * air_speed, this.rigid_body.velocity.y);
+        //}
+        //else if (this.rigid_body.velocity.x <= max_speed && this.rigid_body.velocity.x >= -max_speed)
+        //{
+        //}
+        //this.rigid_body.velocity = new Vector2(move * max_speed, this.rigid_body.velocity.y);
+        if (Input.GetKeyDown("space") && this.grounded)
+            Jump();
+    }
+
+    void Jump ()
+    {
+        this.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 10), ForceMode2D.Impulse);
+    }
+}
---
+++ Assets/Scripts/JellyFish_interaction.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JellyFish_interaction : MonoBehaviour {
+
+    public float bounce_force = 40f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Rigidbody2D hero_rigid = other.GetComponent<Rigidbody2D>();
+            hero_rigid.velocity = new Vector3(hero_rigid.velocity.x, 0f, 0f);
+            //hero_rigid.velocity = new Vector2(hero_rigid.velocity.x, 0f);
+            hero_rigid.AddForce(new Vector3(hero_rigid.velocity.x, bounce_force, 0), ForceMode2D.Impulse);
+        }
+    }
+}
