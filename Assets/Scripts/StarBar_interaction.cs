0aa40981076c7027b7beebb167eadd4d85f697bb
Jonathan Earl <metablue@uw.edu>
2016-05-11 10:33:26 -0700

    debugging starbar

--- Assets/Scripts/StarBar_interaction.cs
+++ Assets/Scripts/StarBar_interaction.cs
@@ -5,7 +5,8 @@ public class StarBar_interaction : MonoBehaviour {
 	private CameraBehavior main_camera;
 	private float width;
 	private float MIN_BAR_WIDTH = 0f;
-	private const float PERCENT_OF_CAMERA = 0.33f;
+	public float PERCENT_OF_CAMERA_WIDTH = 0.33f;
+	public float PERCENT_OF_CAMERA_HEIGHT = 0.3f;
 	private float bar_ratio = 1f;
 	// Use this for initialization
 	void Start () {
@@ -23,13 +24,13 @@ public class StarBar_interaction : MonoBehaviour {
 		float cam_height = main_camera.WorldMax.y - main_camera.WorldMin.y;
 		float cam_width = main_camera.WorldMax.x - main_camera.WorldMin.x;
 		// get width of bar and height of bar
-		float width = cam_width / 2f * PERCENT_OF_CAMERA; //width is 50% of the screen
-		float height = cam_height * 0.05f; // height is 5 % of the screen
+		this.width = cam_width * PERCENT_OF_CAMERA_WIDTH; 
+		float height = cam_height * PERCENT_OF_CAMERA_HEIGHT; // height is 5 % of the screen
 		// get position of bar
 		float x = main_camera.transform.position.x;
 		float y = main_camera.transform.position.y + cam_height/2f - (1f * height);
-
-		this.transform.position = new Vector3 (x, y, 0f);
+		float bar_offset = (1f - this.bar_ratio) * this.width * 1.125f;
+		this.transform.position = new Vector3 (x - bar_offset, y, 0f);
 		this.transform.localScale = new Vector3 (width * this.bar_ratio, height, 0f);
 	}
 
