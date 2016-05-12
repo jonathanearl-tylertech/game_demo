5209e70336870aeb43ab7b2a62b9ae823485e710
Vuochly Ky <ninvk@uw.edu>
2016-05-11 23:17:17 -0700

    Added particle system to the jellyfish tenticles

--- Assets/Scripts/JellyScripts/TentacleColliderInteraction.cs
+++ Assets/Scripts/JellyScripts/TentacleColliderInteraction.cs
@@ -2,15 +2,26 @@
 using System.Collections;
 
 public class TentacleColliderInteraction : MonoBehaviour {
+	ParticleSystem ps = null;
+	bool play_animation = false;
+	float current_time_passed = 0;
+	public float max_time = 1f;
 
 	// Use this for initialization
 	void Start () {
-	
+		ps = this.GetComponent<ParticleSystem> ();
+		ps.Pause ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		this.current_time_passed += Time.deltaTime;
+		if (current_time_passed > max_time && this.play_animation) {
+			//stop animation
+			//ps.Stop();
+			ps.Stop();
+			this.play_animation = false;
+		}
 	}
 
 	void OnCollisionEnter2D(Collision2D other)
@@ -19,6 +30,9 @@ public class TentacleColliderInteraction : MonoBehaviour {
 		{
 			// Code to handle electricity and bouncing out
 			Debug.Log("Meemo touches tentacles");
+			ps.Play ();
+			this.play_animation = true;
+			this.current_time_passed = 0f;
 		}
 	}
 }
